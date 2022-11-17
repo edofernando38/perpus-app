8b7ab3ea603eef5104c2944a8c5c5129fb5b0b63
michaeltanto <112570286+michaeltanto@users.noreply.github.com>
2022-11-17 13:39:39 +0700

    [PA-1-Register] register done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import  {HomePage}  from "./pages/HomePage";
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
+// import { ProfilePage } from "./pages/ProfilePage";
+import { VerificationPage } from "./pages/VerificationPage";
+import  NavbarComp  from "./components/NavbarComponents";
+import Axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "./redux/userSlice";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  // const { id } = useSelector((state) => state.userSlice.value);
+  const token = localStorage.getItem("token");
+  console.log(token);
+
+  const keepLogin = async () => {
+    try {
+      const res = await Axios.get(`http://localhost:2000/users/keepLogin`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(res.NIM);
+      dispatch(login(res.NIM));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    keepLogin();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavbarComp />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/verification/:token" element={<VerificationPage />} />
+      </Routes>
     </div>
   );
 }
